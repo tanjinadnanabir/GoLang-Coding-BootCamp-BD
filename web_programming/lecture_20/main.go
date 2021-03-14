9ba3488f1338fc1d3719981b5608678727986ffe
Tanjin Adnan Abir <tanjinadnanabir@gmail.com>
2021-03-14 11:12:02 +0600

    Update main.go

--- web_programming/lecture_20/main.go
+++ web_programming/lecture_20/main.go
@@ -18,23 +18,30 @@ func init() {
 	// Open up our database connection.
 	// I've set up a database on my local machine using phpmyadmin.
 	// The database is called testDb
-	//db, err = sql.Open("mysql", "root:test123@tcp(127.0.0.1:3306)/hosting_db")
+
+	// db, err = sql.Open("mysql", "root:r00tc0de@tcp(127.0.0.1:3306)/loruki_db")
 
 	// if there is an error opening the connection, handle it
+
 	// if err != nil {
 	// 	panic(err.Error())
 	// }
+
 	// defer the close till after the main function has finished
+
 	// executing
-	//defer db.Close()
-	//fmt.Println("db connection successful")
 
-	//couchbase connection block
+	// defer db.Close()
 
-	db = mcb.Connect("localhost", "mostain", "test123")
+	// fmt.Println("db connection successful")
+
+	// couchbase connection block
+
+	db = mcb.Connect("localhost", "rootcode", "r00tc0de")
+
 	res, err := db.Ping()
-	if err != nil {
 
+	if err != nil {
 		fmt.Println(res)
 		os.Exit(1)
 	}
@@ -55,74 +62,82 @@ func main() {
 func home(w http.ResponseWriter, r *http.Request) {
 
 	ptmp, err := template.ParseFiles("template/base.gohtml")
+
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
 	ptmp.Execute(w, nil)
-	//fmt.Fprintf(w, `welcome`)
 }
 
 func features(w http.ResponseWriter, r *http.Request) {
 
 	ptmp, err := template.ParseFiles("template/base.gohtml")
+
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
 	ptmp, err = ptmp.ParseFiles("wpage/features.gohtml")
+
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	ptmp.Execute(w, nil)
-	//fmt.Fprintf(w, `welcome`)
 }
 
 func docs(w http.ResponseWriter, r *http.Request) {
 
 	ptmp, err := template.ParseFiles("template/base.gohtml")
+
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
 	ptmp, err = ptmp.ParseFiles("wpage/docs.gohtml")
+	
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
 	ptmp.Execute(w, nil)
-	//fmt.Fprintf(w, `welcome`)
 }
 
 // func request(w http.ResponseWriter, r *http.Request) {
 
-// 	//method-1
-// 	name := r.FormValue("name")
-// 	company := r.FormValue("company")
-// 	email := r.FormValue("email")
+	// 	method-1
 
-// 	// fmt.Println(name, company, email)
-// 	// fmt.Fprintf(w, `received %s %s %s`, name, company, email) //response
+	// 	name := r.FormValue("name")
+	// 	company := r.FormValue("company")
+	// 	email := r.FormValue("email")
 
-// 	//method-2
-// 	// r.ParseForm()
-// 	// for key, val := range r.Form {
-// 	// 	fmt.Println(key, val)
-// 	// }
+	// fmt.Println(name, company, email)
 
-// 	qs := "INSERT INTO `request` (`id`, `name`, `company`, `email`, `status`) VALUES (NULL, '%s', '%s', '%s', '1');"
-// 	sql := fmt.Sprintf(qs, name, company, email)
-// 	//fmt.Println(sql)
-// 	insert, err := db.Query(sql)
-// 	if err != nil {
-// 		panic(err.Error())
-// 	}
-// 	defer insert.Close()
+	// fmt.Fprintf(w, `received %s %s %s`, name, company, email) //response
 
-// 	fmt.Fprintf(w, `OK`)
+	//method-2
+
+	// r.ParseForm()
+
+	// for key, val := range r.Form {
+	// 	fmt.Println(key, val)
+	// }
+
+	// 	qs := "INSERT INTO `request` (`id`, `name`, `company`, `email`, `status`) VALUES (NULL, '%s', '%s', '%s', '1');"
+	// 	sql := fmt.Sprintf(qs, name, company, email)
+
+	// 	fmt.Println(sql)
+
+	// 	insert, err := db.Query(sql)
+
+	// 	if err != nil {
+	// 		panic(err.Error())
+	// 	}
+	// 	defer insert.Close()
+
+	// 	fmt.Fprintf(w, `OK`)
 // }
 
-//like mysql table schema
+// like mysql table schema
+
 type RequestTable struct {
 	ID      string `json:"aid"`
 	Name    string `json:"name"`
@@ -135,6 +150,7 @@ type RequestTable struct {
 func requestc(w http.ResponseWriter, r *http.Request) {
 
 	//method-1
+
 	// name := r.FormValue("name")
 	// company := r.FormValue("company")
 	// email := r.FormValue("email")
@@ -143,7 +159,9 @@ func requestc(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, `received %s %s %s`, name, company, email) //response
 
 	//method-2
+
 	r.ParseForm()
+
 	for key, val := range r.Form {
 		fmt.Println(key, val)
 	}
@@ -153,6 +171,7 @@ func requestc(w http.ResponseWriter, r *http.Request) {
 	r.Form.Add("aid", "request::3") //we will update later
 	r.Form.Add("bucket", "master_academy")
 	r.Form.Add("type", "request")
+	
 	pRes := db.Insert(r.Form, &reqTable)
 	fmt.Println(pRes.Status, pRes.Errors)
 
