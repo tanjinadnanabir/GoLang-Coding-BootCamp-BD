5d265a242ecd34a45f9ac6e7cbd919f07f478a31
Tanjin Adnan Abir <tanjinadnanabir@gmail.com>
2021-04-04 13:06:43 +0600

    Update main.go

--- lecture_27/main.go
+++ lecture_27/main.go
@@ -10,8 +10,9 @@ import (
 	"io"
 )
 
-//https://www.thepolyglotdeveloper.com/2018/02/encrypt-decrypt-data-golang-application-crypto-packages/
-//https://www.melvinvivas.com/how-to-encrypt-and-decrypt-data-using-aes/
+// https://www.thepolyglotdeveloper.com/2018/02/encrypt-decrypt-data-golang-application-crypto-packages/
+// https://www.melvinvivas.com/how-to-encrypt-and-decrypt-data-using-aes/
+
 func main() {
 
 	bs := encrypt([]byte("hello"), "test123")
@@ -19,42 +20,53 @@ func main() {
 
 	str := decrypt(bs, "test123")
 	fmt.Println(str, string(str))
-
 }
 
 func encrypt(data []byte, passphrase string) []byte {
+
 	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
 	gcm, err := cipher.NewGCM(block)
+
 	if err != nil {
 		panic(err.Error())
 	}
+
 	nonce := make([]byte, gcm.NonceSize())
+
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
 		panic(err.Error())
 	}
+
 	ciphertext := gcm.Seal(nonce, nonce, data, nil)
 	return ciphertext
 }
 
 func createHash(key string) string {
+
 	hasher := md5.New()
 	hasher.Write([]byte(key))
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
 func decrypt(data []byte, passphrase string) []byte {
+
 	key := []byte(createHash(passphrase))
 	block, err := aes.NewCipher(key)
+
 	if err != nil {
 		panic(err.Error())
 	}
+
 	gcm, err := cipher.NewGCM(block)
+
 	if err != nil {
 		panic(err.Error())
 	}
+
 	nonceSize := gcm.NonceSize()
 	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+
 	if err != nil {
 		panic(err.Error())
 	}
